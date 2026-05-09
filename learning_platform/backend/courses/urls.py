8a8d37dea44760cd416ee443fb43b0d20f38c3ba
Nexanx <barto1903@gmail.com>
2026-05-09 17:56:09 +0200

    tts

--- learning_platform/backend/courses/urls.py
+++ learning_platform/backend/courses/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('kurs<int:course_id>/', views.course_detail, name='course_detail'),
+    path('listen/', views.speak_view, name = 'listen'),
 ]
--- learning_platform/backend/courses/views.py
+++ learning_platform/backend/courses/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from gtts import gTTS
+import io
 
 # Create your views here.
 
 def course_detail(request, course_id):
     return render(request, 'course_detail.html', {'course_id': course_id})
+
+def speak_view(request):
+    text = request.GET.get('text','')
+    
+    if not text:
+        return HttpResponse("Brak tekstu do przeczytania", status = 400)
+    
+    tts = gTTS(text=text, lang='pl')
+    mp3_fp = io.BytesIO()
+    tts.write_to_fp(mp3_fp)
+    mp3_fp.seek(0)
+    
+    return HttpResponse(mp3_fp.read(), content_type="audio/mpeg")
